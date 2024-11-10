fd6145d641ce6d033234edccddcccbb8cb5bf783
Andre Coullard <andrecoullard@gmail.com>
2024-11-10 16:29:12 -0500

    remove 3 templates and renamed the rest

--- src/lib/templates/constants.ts
+++ src/lib/templates/constants.ts
@@ -4,10 +4,6 @@ export const TEMPLATE3 = 3
 export const TEMPLATE4 = 4
 export const TEMPLATE5 = 5
 export const TEMPLATE6 = 6
-export const TEMPLATE7 = 7
-export const TEMPLATE8 = 8
-export const TEMPLATE9 = 9
-export const TEMPLATE10 = 10
 
 export const TEMPLATES = [
   TEMPLATE1,
@@ -16,9 +12,6 @@ export const TEMPLATES = [
   TEMPLATE4,
   TEMPLATE5,
   TEMPLATE6,
-  TEMPLATE7,
-  TEMPLATE8,
-  TEMPLATE9
 ]
 
 export const NEWLINE = '\\\\'
