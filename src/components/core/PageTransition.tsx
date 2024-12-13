d9af0e7dade29afe091e0f87bc577763bfe00a3c
Jason <jasonbalayev@gmail.com>
2024-12-12 21:31:00 -0500

    Test

--- src/components/core/PageTransition.tsx
+++ src/components/core/PageTransition.tsx
@@ -1,1 +1,23 @@
- 
+import { motion } from 'framer-motion'
+import styled from 'styled-components'
+
+const TransitionWrapper = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: #121212;
+  z-index: 999;
+`
+
+export const PageTransition = () => (
+  <TransitionWrapper
+    initial={{ opacity: 1 }}
+    animate={{ opacity: 0 }}
+    exit={{ opacity: 1 }}
+    transition={{ duration: 0.3, ease: 'easeInOut' }}
+  />
+)
+
+export {} 
